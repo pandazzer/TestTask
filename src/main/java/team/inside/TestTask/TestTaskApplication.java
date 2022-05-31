b12578c65b2023d7b83f68750e0e5c062ea451bf
dmitry <dmitry.mikhailov@devim.team>
2022-05-31 14:21:39 +0300

    update3

--- src/main/java/team/inside/TestTask/TestTaskApplication.java
+++ src/main/java/team/inside/TestTask/TestTaskApplication.java
@@ -2,13 +2,8 @@ package team.inside.TestTask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import javax.sql.DataSource;
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@EnableJpaRepositories("team.inside.TestTask.Repository")
+@SpringBootApplication()
 public class TestTaskApplication {
 
 	public static void main(String[] args) {
